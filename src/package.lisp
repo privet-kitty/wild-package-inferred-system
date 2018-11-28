7fa2a451b204ddc189805492dbbf2928166b3d6f
privet-kitty <privet.kitty99@gmail.com>
2018-11-29 00:26:18 +0100

    Unuse asdf/session

--- src/package.lisp
+++ src/package.lisp
@@ -1,8 +1,8 @@
 (uiop:define-package :wild-package-inferred-system
     (:nicknames :wpis)
-  (:use :cl :uiop :asdf/upgrade :asdf/session :asdf/operation
+  (:use :cl :uiop :asdf :asdf/upgrade :asdf/operation
         :asdf/component :asdf/system :asdf/system-registry :asdf/lisp-action
         :asdf/parse-defsystem :asdf/package-inferred-system
-        :asdf/output-translations :asdf/session :asdf/operate)
+        :asdf/output-translations :asdf/operate)
   (:export #:wild-package-inferred-system
            #:empty-wild-system))
