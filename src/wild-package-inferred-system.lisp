ec41bdec03d764130ac4c1a6cbfaf9b57f4b4c7c
privet-kitty <privet.kitty99@gmail.com>
2018-11-29 14:55:12 +0100

    [ci skip] typo

--- src/wild-package-inferred-system.lisp
+++ src/wild-package-inferred-system.lisp
@@ -185,7 +185,7 @@ and .script.lisp, even if they match a given wild card."
 (pushnew 'sysdef-wild-package-inferred-system-search *system-definition-search-functions*)
 
 (defun reduce-all-wild-packages (primary-name &optional (delete t))
-  "Reducts all wild packages beginning with PRIMARY-NAME"
+  "Reduces all wild packages beginning with PRIMARY-NAME"
   (unless (primary-system-p primary-name)
     (error "~S must be primary name." primary-name))
   (let ((primary-name (standard-case-symbol-name primary-name)))
