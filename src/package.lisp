eb21c111a54dea30db9f070202cbfa30856014d0
privet-kitty <privet.kitty99@gmail.com>
2018-11-29 02:28:04 +0100

    Make the imported symbols from ASDF internal clear

--- src/package.lisp
+++ src/package.lisp
@@ -1,8 +1,11 @@
 (uiop:define-package :wild-package-inferred-system
     (:nicknames :wpis)
-  (:use :cl :uiop :asdf :asdf/upgrade :asdf/operation
-        :asdf/component :asdf/system :asdf/system-registry :asdf/lisp-action
-        :asdf/parse-defsystem :asdf/package-inferred-system
-        :asdf/output-translations :asdf/operate)
+  (:use :cl :uiop :asdf)
+  (:import-from :asdf
+                #:basic-load-op
+                #:primary-system-p
+                #:*defpackage-forms*
+                #:*package-inferred-systems*
+                #:around-compile-hook)
   (:export #:wild-package-inferred-system
            #:empty-wild-system))
